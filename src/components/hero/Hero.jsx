4021c2612c943567f5c02b8af4a92cd0e3310237
Salsabila Balqis <salsabilabalqis1012@gmail.com>
2023-10-14 16:40:51 +0700

    add hero component

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "./hero.css";
+import heroImg from "./../../assets/img/hero-bg.jpg";
+import heroInfoImg from "./../../assets/img/hero-info-img.jpg";
+import { FaFacebookF } from "react-icons/fa";
+import { BsInstagram, BsTwitter } from "react-icons/bs";
+import { ButtonPrimary, ButtonLinkArrow } from "../button/Button";
+
+function Hero() {
+  return (
+    <section className="hero" id="hero">
+      <img src={heroImg} alt="" className="hero_img" />
+
+      <div className="hero_container container grid">
+        <div className="hero_data">
+          <span className="hero_data-subtitle">Discover your place</span>
+          <h1 className="hero_data-title">
+            Explore Indonesia's Iconic Destinations
+          </h1>
+          <p className="hero_data-desc">
+            From Bali's golden beaches to Yogyakarta's ancient temples, embark
+            on a journey through Indonesia's most enchanting locales.{" "}
+          </p>
+          <ButtonPrimary text={"Explore"} />
+        </div>
+
+        <div className="hero_social">
+          <a href="https://www.facebook.com/" className="hero_social-link">
+            <FaFacebookF />
+          </a>
+          <a href="https://www.instagram.com/" className="hero_social-link">
+            <BsInstagram />
+          </a>
+          <a href="https://twitter.com/" className="hero_social-link">
+            <BsTwitter />
+          </a>
+        </div>
+
+        <div className="hero_info">
+          <div>
+            <span className="hero_info-title">5 best places to visit</span>
+            <ButtonLinkArrow text={"More"} style={{ color: "white" }} />
+          </div>
+
+          <div className="hero_info-overlay">
+            <img src={heroInfoImg} alt="" className="hero_info-img" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
